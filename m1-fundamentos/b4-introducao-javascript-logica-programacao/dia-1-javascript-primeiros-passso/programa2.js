814299e982f7e708813af997f44ecf5e26ee40e3
Andre Felipe <afelipes@gmail.com>
2021-11-28 09:56:32 -0300

    Programa2 - Maior entre 2 numeros

---
+++ m1-fundamentos/b4-introducao-javascript-logica-programacao/dia-1-javascript-primeiros-passso/programa2.js
@@ -0,0 +1,11 @@
+const a = 5;
+const b = 3;
+let mensagem;
+
+// programa que calcula qual é o maior entre 2 numeros;
+
+if (a>b){
+    console.log("\nO número (a)",a,"é maior que (b)",b);
+}else{
+    console.log("\nO número (b)",b,"é maior que (a)",a);
+}
