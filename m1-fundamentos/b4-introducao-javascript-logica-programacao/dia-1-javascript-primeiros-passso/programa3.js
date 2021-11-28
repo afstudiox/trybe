ab86673d9a46994f6a8789369ee2e9e0aa48e9c3
Andre Felipe <afelipes@gmail.com>
2021-11-28 10:05:08 -0300

    Programa3 - Maior entre 3 numeros

---
+++ m1-fundamentos/b4-introducao-javascript-logica-programacao/dia-1-javascript-primeiros-passso/programa3.js
@@ -0,0 +1,13 @@
+const a = 8;
+const b = 9;
+const c = 7;
+
+// programa que calcula qual é o maior entre 3 numeros;
+
+if (a>b && a>c){
+    console.log("\nO número (a)",a,"é maior que (b)",b,"é maior que (c)",c);
+}else if (b>a && b>c){
+    console.log("\nO número (b)",b,"é maior que (a)",a,"é maior que (c)",c);
+}else if (c>a && c>b){
+    console.log("\nO número (c)",c,"é maior que (a)",a,"é maior que (b)",b);
+}
