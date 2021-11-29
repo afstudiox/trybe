a68d0049054c288345a25a8abe993668f5a93866
Andre Felipe <afelipes@gmail.com>
2021-11-29 15:36:09 -0300

    Programa 9 - Calcula o lucro da empresa na venda de 1.000 unidades do produto

--- m1-fundamentos/b4-introducao-javascript-logica-programacao/dia-1-javascript-primeiros-passso/programa8.js
+++ m1-fundamentos/b4-introducao-javascript-logica-programacao/dia-1-javascript-primeiros-passso/programa8.js
@@ -1,8 +1,8 @@
-const a = 10;
-const b = 3;
-const c = 4;
+const a = 2;
+const b = 4;
+const c = 6;
 
-if (a % 2 != 0 || b % 2 != 0 || c % 2 != 0 ){
+if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0 ){
     console.log(true);
 } else {
     console.log(false);
