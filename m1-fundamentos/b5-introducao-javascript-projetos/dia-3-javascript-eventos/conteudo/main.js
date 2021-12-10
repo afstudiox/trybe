73880de5e5fcedd1b78c3d2bc96a38ad9a86d9ec
Andre Felipe <afelipes@gmail.com>
2021-12-10 17:17:07 -0300

    Calendário Trybe - Exercicio 1

--- m1-fundamentos/b5-introducao-javascript-projetos/dia-3-javascript-eventos/conteudo/main.js
+++ m1-fundamentos/b5-introducao-javascript-projetos/dia-3-javascript-eventos/conteudo/main.js
@@ -10,66 +10,48 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre? 
 // POR CAUSA DA CLASSE .tech QUE TEM EM SUA CONFIGURAÇÃO UM DESLOCAMENTO
 
-//=======================================================================================================================================================
-
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
-// captura todas as <li> do documento e adciona na variavel 'li' 
 let li = document.getElementsByTagName('li');
-// função que remove a classe 'tech' de todas as <li>
+
 function resetTech(){
   for (let i = 0; i < li.length; i += 1){
     li[i].classList.remove('tech');
   }
 }
-// funcão que adiciona a classe 'tech' no objeto acionado pelo 'click' 
+
 function swapTech(origin){
   resetTech();
   origin.target.classList.add('tech');
 }
-//loop que monitora o clique em qualquer item da li
+
+// monitora o click em qualquer uma das <li>
 for (let i = 0; i < li.length; i += 1){
   li[i].addEventListener('click',swapTech);
 }
 
-//=======================================================================================================================================================
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
-// define que tudo que for digitado no objeto acionado pelo 'keypress' seja adicionado na primeira li no 'innerText'
 function changeText(origin){
   firstLi.innerText = origin.target.value;
 }
-// monitora a pressionamento de teclha no <input>
-input.addEventListener('keypress', changeText);
 
-//=======================================================================================================================================================
+input.addEventListener('keypress', changeText);
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
+function openPage(origin){
+  myWebpage =   
+}
 
-myWebpage.addEventListener('dblclick',function(){
-  window.open('http://www.afstudio.com.br');
-});
-
-
-//=======================================================================================================================================================
+myWebpage.addEventListener('dblclick',openPage)
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
-
-myWebpage.addEventListener('mouseover',function(origin){
-  origin.target.style.color='yellow';
-});
-
-myWebpage.addEventListener('mouseout',function(origin){
-  origin.target.style.color='white';
-});
-
-
 
 // Segue abaixo um exemplo do uso de event.target:
 
