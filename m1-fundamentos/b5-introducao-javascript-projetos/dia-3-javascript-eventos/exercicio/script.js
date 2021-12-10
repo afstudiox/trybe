ba61c2165193ad1851d577283941d4cc2cec3fcf
Andre Felipe <afelipes@gmail.com>
2021-12-10 18:41:08 -0300

    Calendário Trybe - Exercicio 3

--- m1-fundamentos/b5-introducao-javascript-projetos/dia-3-javascript-eventos/exercicio/script.js
+++ m1-fundamentos/b5-introducao-javascript-projetos/dia-3-javascript-eventos/exercicio/script.js
@@ -22,32 +22,34 @@ Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a clas
 */
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
+// arrays dos feriado e sextas-feiras
 const holyday=[24,25,31];
 const friday=[4,11,18,25];
 
+//localiza a classe pai que será inserido os dias
 const days = document.querySelector('#days');
 
+// loop para construir os dias 
 for (let i=0; i < dezDaysList.length; i += 1){
   const day = document.createElement('li');
   day.classList.add('day');
   day.innerHTML=dezDaysList[i];
   days.appendChild(day)
+
+  //loop verifica se é feriado
   for(let j=0; j < holyday.length; j += 1){
     if(day.innerText == holyday[j]){
       day.classList.add('holiday');
     }
   }
+
+  //loop verifica se é sexta-feira
   for (let j=0; j < friday.length; j += 1){
     if (day.innerText == friday[j]){
       day.classList.add('friday');
     }
   }
 }
-
-
-
-
-
 
 // Exercício 2
 /*
@@ -62,3 +64,29 @@ btnHoliday.innerHTML='Feriados';
 const btnContainer = document.querySelector('.buttons-container');
 btnContainer.appendChild(btnHoliday);
 
+// Exercicio 3
+/* 
+Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que possuem a classe "holiday" .
+É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial com a cor "rgb(238,238,238)".
+*/
+
+// colocando uma escuta no botao 'holiday'
+btnHoliday.addEventListener('click',fillHolyday);
+let check = false;
+
+function fillHolyday(origin){
+  let holydayDays = document.querySelectorAll('.holiday'); 
+  if (check == false){
+    for (let i = 0; i < holydayDays.length; i += 1){
+        holydayDays[i].style.backgroundColor='yellow';
+        origin.target.style.backgroundColor='yellow';
+    }
+    check = true;
+  }else{
+    for (let i = 0; i < holydayDays.length; i += 1){
+      holydayDays[i].style.backgroundColor='rgb(238,238,238)';
+      origin.target.style.backgroundColor='rgb(238,238,238)';
+  }
+  check = false;
+  }
+}
