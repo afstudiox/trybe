f83883f95614c1125e4b7514ae81de64e7f43527
Andre Felipe <afelipes@gmail.com>
2022-01-05 21:10:26 -0300

    HTML e CSS Completo para aplicar JS

--- m1-fundamentos/b6-html-css-forms-flexbox-responsivo/d1-html-css-forms/exercicios/script.js
+++ m1-fundamentos/b6-html-css-forms-flexbox-responsivo/d1-html-css-forms/exercicios/script.js
@@ -1,5 +1,6 @@
 // funcao para criar o menu de estados 
 const uf = document.querySelector('#uf');
+const date = document.querySelector('#dt-inicio');
 
 function criaUF(){
     let ufBrasil = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins']
@@ -11,3 +12,13 @@ function criaUF(){
     }
 }
 
+function checkDate(){
+   console.log(Date.parse(date.value));
+   console.log(Date.parse(date.value) instanceof Date && !isNaN(Date.parde(date.value)));
+}
+
+
+window.onload = function(){
+    criaUF();
+    checkDate();
+}
