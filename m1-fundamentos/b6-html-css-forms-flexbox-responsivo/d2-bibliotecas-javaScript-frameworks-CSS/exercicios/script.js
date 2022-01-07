bdc2259a52f12d099f23441606c08cd407a398d1
Andre Felipe <afelipes@gmail.com>
2022-01-07 15:49:50 -0300

    conteudo dia 6.4 primeira parte

--- m1-fundamentos/b6-html-css-forms-flexbox-responsivo/d2-bibliotecas-javaScript-frameworks-CSS/exercicios/script.js
+++ m1-fundamentos/b6-html-css-forms-flexbox-responsivo/d2-bibliotecas-javaScript-frameworks-CSS/exercicios/script.js
@@ -1,6 +1,5 @@
 // funcao para criar o menu de estados 
 const uf = document.querySelector('#uf');
-const date = document.querySelector('#dt-inicio');
 
 function criaUF(){
     let ufBrasil = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins']
@@ -12,13 +11,7 @@ function criaUF(){
     }
 }
 
-function checkDate(){
-   console.log(Date.parse(date.value));
-   console.log(Date.parse(date.value) instanceof Date && !isNaN(Date.parde(date.value)));
-}
-
 
 window.onload = function(){
     criaUF();
-    checkDate();
 }
