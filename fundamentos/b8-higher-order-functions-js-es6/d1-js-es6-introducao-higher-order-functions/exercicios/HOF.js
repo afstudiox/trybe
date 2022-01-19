90005da63799bd9e9c1b532eefac99226bf3f785
Andre Felipe <afelipes@gmail.com>
2022-01-19 15:44:53 -0300

    Exercicio dia 8.1 - Imcompleto | Push para inicializar novo dia

--- fundamentos/b8-higher-order-functions-js-es6/d1-js-es6-introducao-higher-order-functions/exercicios/HOF.js
+++ fundamentos/b8-higher-order-functions-js-es6/d1-js-es6-introducao-higher-order-functions/exercicios/HOF.js
@@ -25,3 +25,27 @@ const sorteio = (aposta) => {
 }; 
 
 // console.log(sorteio(2));
+
+//3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+
+//Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
+
+const gabarito = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const resposta = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+let resultado = 0;
+
+const check = (arr1,arr2) => {
+  for (let index = 1; index <= gabarito.length; index += 1){
+    if (arr1[index] === arr2[index]){
+      resultado = resultado + 1;
+    }else if(arr2[index] === 'N.A'){
+      resultado = resultado;
+    }
+    resultado = resultado - 0.5;
+  }
+  return resultado;
+}
+
+check(gabarito,resposta);
+console.log(resultado);
+
