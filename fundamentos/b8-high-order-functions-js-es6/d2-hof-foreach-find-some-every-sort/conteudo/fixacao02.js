00d160bf429ec74af65b3d5df342a5b8325c142b
Andre Felipe <afelipes@gmail.com>
2022-01-21 18:48:38 -0300

    Dia 8.2 | Fixação 1

--- fundamentos/b8-high-order-functions-js-es6/d2-hof-foreach-find-some-every-sort/conteudo/fixacao02.js
+++ fundamentos/b8-high-order-functions-js-es6/d2-hof-foreach-find-some-every-sort/conteudo/fixacao02.js
@@ -1,10 +1,10 @@
-const numbers = [19, 21, 30, 3, 45, 22, 15];
+const numbers = [19, 21, 31, 3, 45, 22, 15];
 
-const findDivisibleBy3And5 = () => {
-  return numbers.find((number) => {
-  return (number % 3 === 0) && (number % 5 === 0);
-})
-};
+//funcao para verificar se é divisível
+const isDivisible = (item) => (item % 3 === 0) && (item % 5 === 0);
 
+//hof usando find para encontrar o critério que seja true
+const findDivisibleBy3And5 = numbers.find((item) => isDivisible(item));
 
-console.log(findDivisibleBy3And5());
+//exibindo resultado na tela
+console.log(findDivisibleBy3And5);
