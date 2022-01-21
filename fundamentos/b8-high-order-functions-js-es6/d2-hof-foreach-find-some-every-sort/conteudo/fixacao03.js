31576b50ba0fe0f18d15577a961ef36aaa902acf
Andre Felipe <afelipes@gmail.com>
2022-01-21 18:58:38 -0300

    Dia 8.2 | Find > Fixação 2

--- fundamentos/b8-high-order-functions-js-es6/d2-hof-foreach-find-some-every-sort/conteudo/fixacao03.js
+++ fundamentos/b8-high-order-functions-js-es6/d2-hof-foreach-find-some-every-sort/conteudo/fixacao03.js
@@ -1,7 +1,12 @@
 const names = ['João', 'Irene', 'Fernando', 'Maria'];
 
+//funcao que verifica o tamanho do nome 
 const sizeName = (name) => name.length === 5; 
 
+//HOF que retorna o primeiro nome com o tamanho informado na funcao sizeName
 const findNameWithFiveLetters = () => names.find(sizeName);
 
 console.log(findNameWithFiveLetters());
+
+
+//obs.: nessa HOF foi retornada uma função, porém não foi passada nenhuma função como parametro.
