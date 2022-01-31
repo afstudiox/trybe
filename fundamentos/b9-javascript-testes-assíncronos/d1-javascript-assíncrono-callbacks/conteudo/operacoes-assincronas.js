02ce2df22ad5ff735328d64ce164a62e0eb9bdd5
Andre Felipe <afelipes@gmail.com>
2022-01-31 18:06:08 -0300

    Dia 9.1 - Exercicios de Fixação INCOMPLETOS

---
+++ fundamentos/b9-javascript-testes-assíncronos/d1-javascript-assíncrono-callbacks/conteudo/operacoes-assincronas.js
@@ -0,0 +1,26 @@
+const pushNumber = (list, number) => list.push(number);
+
+let numbers = [];
+
+pushNumber(numbers, 1);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+console.log(numbers);
+
+numbers = [];
+
+setTimeout(() => pushNumber(numbers, 1), 3000);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+console.log(numbers); // O retorno é [2, 3]
+
+
+numbers = [];
+
+setTimeout(() => pushNumber(numbers, 1), 5000);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+setTimeout(() => console.log(numbers), 5000);
