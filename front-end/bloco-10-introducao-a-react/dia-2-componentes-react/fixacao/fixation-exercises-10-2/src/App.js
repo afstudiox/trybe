ae59937cbbbd45a742694d1b4ddc0e3426bda302
Andre Felipe <afelipes@gmail.com>
2022-02-17 18:36:24 -0300

    Exercício de fixação fixation-exercicses-10-2

---
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixacao/fixation-exercises-10-2/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Image from './Image'
+
+function App() {
+  return (
+  <img src='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg'
+       alt='Cute cat staring'/>
+  );
+}
+
+export default App;
