74af70d857b12241228d0ec7b4a26e5e02b17f20
Andre Felipe <afelipes@gmail.com>
2022-06-07 22:10:55 -0300

    encerrando o dia

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/exercicio/imc.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/exercicio/imc.js
@@ -4,22 +4,22 @@ const calcIMC = () => {
 
   const peso = readline.questionFloat('Digite seu peso: ');
   const altura = readline.questionFloat('Digite sua altura: ');
-  const imc = peso / altura ** 2; 
+  const imc = peso / (altura ** 2); 
 
   switch (true) {
     case imc < 18.5:
       console.log('Abaixo do peso (magreza)')
       break;
-    case imc >= 18.5 || imc <= 24.9:
+    case imc >= 18.5 && imc <= 24.9:
       console.log('Peso normal')
       break;
-    case imc >= 25 || imc <= 29.9:
+    case imc >= 25 && imc <= 29.9:
       console.log('Acima do peso (sobrepeso)')
       break;
-    case imc >= 30 || imc <= 34.9:
+    case imc >= 30 && imc <= 34.9:
       console.log('Obesidade grau I')
       break;
-    case imc >= 35 || imc <= 39.9:
+    case imc >= 35 && imc <= 39.9:
       console.log('Obesidade grau II')
       break;
     case imc >= 40:
