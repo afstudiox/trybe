fdefdddbd38a270a5e593e98c291c65d319dc328
Andre Felipe <afelipes@gmail.com>
2022-06-08 23:41:34 -0300

    exercicio 4 - completo

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicios/exercicio4.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicios/exercicio4.js
@@ -21,8 +21,58 @@ const readAll = async () => {
   strings.forEach((string) => console.log(string));
 }
 
-const main = ()=> {
-  readAll();
+const getSimpsonById = async (id) => {
+  const fileContent = await fs.readFile('./simpsons.json', 'utf-8')
+  const simpsons = JSON.parse(fileContent);
+  const chosenSimpson = simpsons.find((simpson) => Number(simpson.id) == id);
+
+  if(!chosenSimpson) {
+    throw new Error('Id não encontrado');
+  }
+
+  return chosenSimpson;
+}
+
+const filterSimpson = async () => {
+  const fileContent = await fs.readFile('./simpsons.json','utf-8')
+  const simpsons = JSON.parse(fileContent);
+  const newArray = simpsons
+    .filter((simpson) => simpson.id != '10' && simpson.id != '6');
+  await fs.writeFile('./simpsons.json', JSON.stringify(newArray));
+}
+
+const createSimpsonsFamily = async () => {
+  const fileContent = await fs.readFile('./simpsons.json','utf-8')
+  const simpsons = JSON.parse(fileContent);
+  const familyIds = [1, 2, 3, 4];
+  const simpsonsFamily = simpsons.filter((simpson) => familyIds.includes(Number(simpson.id)))
+  await fs.writeFile('./simpsonsFamily.json', JSON.stringify(simpsonsFamily));
+}
+
+const addNelsonToFamily = async () => {
+  const fileContent = await fs.readFile('./simpsonsFamily.json','utf-8')
+  const simpsonsFamily = JSON.parse(fileContent);
+  simpsonsFamily.push({id:'8', name:'Nelson Muntz'});
+  await fs.writeFile('./simpsonsFamily.json', JSON.stringify(simpsonsFamily));
+}
+
+const swapPerson = async () => {
+  const maggieUpdate = {id: '8', name:'Maggie Simpson'};
+  const fileContent = await fs.readFile('./simpsonsFamily.json', 'utf-8')
+  const simpsons = JSON.parse(fileContent)
+  const filterSimpson = simpsons.filter((simpson) => simpson.id !== '8')
+  const updateSimpsons = [...filterSimpson, maggieUpdate];
+  return fs.writeFile('./simpsonsFamily.json', JSON.stringify(updateSimpsons));  
+}
+
+const main = async () => {
+  swapPerson();
+  // addNelsonToFamily();
+  // createSimpsonsFamily();
+  // filterSimpson();
+  // const simpson = await getSimpsonById(1);
+  // console.log(simpson);
+  // readAll();
 }
 
 main();
