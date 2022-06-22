a28f04d3935a63922adf8888ddc3ea7649f88507
Andre Felipe <afelipes@gmail.com>
2022-06-22 11:39:49 -0300

    feat : Criando uma rota POST /greetings :rocket:

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicios/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicios/index.js
@@ -21,4 +21,14 @@ app.post('/hello', (req,res) => {
   res.status(200).json({ message: `Hello, ${name}`});
 })
 
+// Crie uma rota POST /greetings 🚀
+// Sua rota deve receber o seguinte JSON: { "name": "<nome do usuário>", "age": <idade do usuário> }.
+// Caso a pessoa usuária tenha idade superior a 17 anos, devolva o JSON { "message": "Hello, <nome do usuário>!" } com o status code 200 - OK.
+// Caso a pessoa usuária tenha 17 anos ou menos, devolva o JSON { "message": "Unauthorized" } com o status code 401 - Unauthorized.
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+  if (!age || age < 18) return res.status(401).json({ message: 'Unauthorized'});
+  res.status(200).json({ message: `Hello, ${name} !`});
+})
+
 app.listen(PORT, () => console.log('Rodando na porta 3002'));
