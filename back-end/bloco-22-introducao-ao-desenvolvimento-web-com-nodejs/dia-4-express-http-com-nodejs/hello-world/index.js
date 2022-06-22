5ebdf62078082914fb84c0571aa626ef98ac783c
Andre Felipe <afelipes@gmail.com>
2022-06-22 10:31:58 -0300

    feat : concluído todos os exercicios de fixação :roquet:

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/hello-world/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/hello-world/index.js
@@ -22,6 +22,12 @@ const drinks = [
 	{ id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
 ];
 
+// Rota Geral
+app.get('/', (_req, res) => {
+  res.status(200).json({ message: 'Você esta na rota principal!'});
+});
+
+
 // Rotas GET
 app.get('/recipes', function (req, res) {
   res.json(recipes);
@@ -31,30 +37,36 @@ app.get('/drinks', function (req, res) {
   res.json(drinks);
 });
 
+app.get('/validateToken', (req, res) => {
+  const token = req.headers.authorization;
+  if (token.length !== 16) {
+    return res.status(400).json({ message: 'Invalid Token !'});
+  }
+  res.status(200).json({ message: 'Valid Token'});
+})
+
 app.get('/recipes/search', function(req, res) {
   const { name, maxPrice, minPrice } = req.query;
   const filteredRecipes = recipes
-    .filter((recipes) => recipes.name
-    .includes(name) && 
-    recipes.price < Number(maxPrice) &&
-    recipes.price >= Number(minPrice));
+  .filter((recipes) => recipes.name
+  .includes(name) && 
+  recipes.price < Number(maxPrice) &&
+  recipes.price >= Number(minPrice));
   res.status(200).json(filteredRecipes);
-});
-
-app.get('/recipes/:id', function (req, res) {
-  const { id } = req.params;
-  const recipe = recipes.find((recipe) => recipe.id === Number(id));
-
-  if (!recipe) return res.status(404).json({message: 'Recipe not Found'}); 
-
-  res.status(200).json(recipe);
 });
 
 app.get('/drinks/search', function(req,res) {
   const { name } = req.query;
   const filteredDrinks = drinks
-    .filter((drink) => drink.name.includes(name));
+  .filter((drink) => drink.name.includes(name));
   res.status(200).json(filteredDrinks);
+});
+
+app.get('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const recipe = recipes.find((recipe) => recipe.id === Number(id));
+  if (!recipe) return res.status(404).json({message: 'Recipe not Found'}); 
+  res.status(200).json(recipe);
 });
 
 app.get('/drinks/:id', function (req, res) {
@@ -77,7 +89,45 @@ app.post('/drinks', function (req, res) {
   res.status(201).json({ message: 'Drink created successfully!'});
 });
 
+// Rotas PUT
+app.put('/recipes/:id',  (req, res) => {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const recipeIndex = recipes.findIndex((recipe) => recipe.id === Number(id));
+  if (recipeIndex === -1)return res.status(404).json({ message: 'Recipe not found !'});
+  recipes[recipeIndex] = {...recipes[recipeIndex], name, price };
+  res.status(204).end();
+});
 
+app.put('/drinks/:id',  (req, res) => {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const drinkIndex = drinks.findIndex((drink) => drink.id === Number(id));
+  if (drinkIndex === -1)return res.status(404).json({ message: 'Drink not found !'});
+  drinks[drinkIndex] = {...drinks[drinkIndex], name, price };
+  res.status(204).end();
+});
+
+app.delete('/recipes/:id', (req, res) => {
+  const { id } = req.params;
+  const recipeIndex = recipes.findIndex((recipe) => recipe.id === Number(id));
+  if (recipeIndex === -1) return res.status(404).json({ message: 'Recipe not found' });
+  recipes.splice(recipeIndex, 1);
+  res.status(204).end();
+});
+
+app.delete('/drinks/:id',  (req, res) => {
+  const { id } = req.params;
+  const drinkIndex = drinks.findIndex((drink) => drink.id === Number(id));
+  if (drinkIndex === -1)return res.status(404).json({ message: 'Drink not found !'});
+  drinks.splice(drinkIndex, 1); // ENTENDER MELHOR ESSE SPLICE
+  res.status(204).end();
+});
+
+// Rotas Erradas
+app.all('*', (req, res) => {
+  return res.status(404).json({ message: `Rota ${req.path} não existe`});
+})
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001!');
