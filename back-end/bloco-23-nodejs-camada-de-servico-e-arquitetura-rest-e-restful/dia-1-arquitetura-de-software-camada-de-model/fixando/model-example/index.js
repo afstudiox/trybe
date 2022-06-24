138a79d97c5b11249010e2803df0801aba7276df
Andre Felipe <afelipes@gmail.com>
2022-06-24 19:00:50 -0300

    feat : Fixando - Buscando pelos detalhes de uma pessoa escritora :rocket:

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/fixando/model-example/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/fixando/model-example/index.js
@@ -13,6 +13,13 @@ app.get('/authors', async (_req, res) => {
 	res.status(200).json(authors);
 });
 
+app.get('/authors/:id', async (req, res) => {
+  const { id } = req.params;
+  const author = await Author.findById(id);
+  if (!author) return res.status(400).json({ message: 'Not found' });
+  res.status(200).json(author);
+})
+
 app.get('/books', async (req, res) => {
   const { author_id } = req.query;
   
