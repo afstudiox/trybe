29cb6accdecbf9b7d062f9e5d4615b2c8b47ad46
Andre Felipe <afelipes@gmail.com>
2022-06-29 07:42:14 -0300

    :tada: feat: Exercício 1 - Implantada a rota  getCEP , a rota que busca o cep passado por parametro (localhost:3000/cep/parametro)

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/src/Controllers/cepController.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/src/Controllers/cepController.js
@@ -1,11 +1,16 @@
+// concexao com o service
 const cepService = require('../Services/cepService');
 
 const cepController = {
+  // envia para o service o cep por parametro 
+  // retorna para o cliente o item encontrado no banco
   async get(req, res) {
     const { cep } = req.params;
     const item = await cepService.get(cep);
     res.json(item);
   },
+  // envia para para o service a solicitacao de busca de todos os ceps
+  // retorna para o cliente os items cadastrados no banco
   async getAll(_req, res) {
     const items = await cepService.getAll();
     res.json(items);
--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/src/Services/cepService.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/src/Services/cepService.js
@@ -1,11 +1,15 @@
+// conexao com o model 
 const cepModel = require('../Models/cepModel');
 
 const cepService = {
+  // solicita ao model a busca do cep no banco
+  // retorna a resposta do model para o controller
   async get(cep) {
     const item = await cepModel.get(cep);
     return item;
   },
-
+  // solicita ao model a busca de todos os ceps cadastrados no banco 
+  // retorna a resposta do model para o controller
   async getAll() {
     const items = await cepModel.getAll();
     return items;
