d01c84c3037012eeaac642c30440db23478e97a4
Andre Felipe <afelipes@gmail.com>
2022-06-29 07:42:14 -0300

    :tada: feat: Exercício 2 - Implantada a rota  getCEP , a rota que busca o cep passado por parametro (localhost:3000/cep/parametro)

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/src/Models/cepModel.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/src/Models/cepModel.js
@@ -1,11 +1,16 @@
+// conexao com o banco de dados
 const db = require('./connection');
 
 const cepModel = {
+  // busca cep por ID direatmente no banco de dados
+  // retorna o item encontrado para o service
   async get(cep) {
     const sql = 'SELECT * FROM ceps WHERE cep = ?';
     const [[item]] = await db.query(sql, [cep]);
     return item;
   },
+  // busca todos os CEPS cadastrados diretamente no banco de dados 
+  // retorna os items encontrados para o service
   async getAll() {
     const sql = 'SELECT * FROM ceps';
     const [items] = await db.query(sql);
--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/src/Routes/cepRoutes.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/src/Routes/cepRoutes.js
@@ -1,6 +1,9 @@
+// importa a lib de rotas
 const { Router } = require('express');
+// conexao com o controller
 const cepController = require('../Controllers/cepController');
 
+// define a constante de rotas do CEP
 const cepRoutes = Router();
 
 // removeCEP
@@ -10,12 +13,16 @@ cepRoutes.delete('/:cep', async (_req, _res) => { });
 cepRoutes.put('/:cep', async (_req, _res) => { });
 
 // getCEP
+// rota que busca o cep passado por parametro
+// localhost:3000/cep/parametro
 cepRoutes.get('/:cep', cepController.get);
 
 // addCEP
 cepRoutes.post('/', async (_req, _res) => { });
 
 // listCEP
+// rota que busca todos os ceps cadastrados
+// localhost:3000/
 cepRoutes.get('/', cepController.getAll);
 
 module.exports = cepRoutes;
