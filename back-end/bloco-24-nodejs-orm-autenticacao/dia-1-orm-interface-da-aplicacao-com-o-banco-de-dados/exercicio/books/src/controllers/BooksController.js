bc562e9160d47f9602cad34b3722833551fa9449
Andre Felipe <afelipes@gmail.com>
2022-07-08 12:04:58 -0300

    feat: Exercício 7: No controller BooksController crie o método getById sendo um middleware que recebe o id como parâmetro de rota e buscar o livro por meio do service. Se o livro não existir a resposta da requisição deve ter o status 404 e o json { "message": "Book not found" }.

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio/books/src/controllers/BooksController.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio/books/src/controllers/BooksController.js
@@ -4,6 +4,12 @@ const booksController = {
   list: async (req, res) => {
     const books = await booksService.list;
     res.status(200).json(books);
+  },
+  getById: async (req,res) => {
+    const { id } = req.params;
+    const book = await booksService.getById(id);
+    if (!book) return res.status(404).json({ message: "Book not found"});
+    res.status(200).json(book);
   }
 }
 
