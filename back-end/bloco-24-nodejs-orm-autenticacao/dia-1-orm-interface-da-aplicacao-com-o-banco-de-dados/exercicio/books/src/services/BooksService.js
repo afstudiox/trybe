f79d35934f5429bcc1153cc36493e73abf409ecb
Andre Felipe <afelipes@gmail.com>
2022-07-08 11:58:07 -0300

    feat: Exercício 6: No service BooksService crie um método getById que recebe um id como parâmetro e use o model Book para buscar esse livro. Retorne o objeto encontrado pelo model.

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio/books/src/services/BooksService.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio/books/src/services/BooksService.js
@@ -4,6 +4,9 @@ const booksServices = {
   list: async () => {
     const books = await Book.findAll();
     return books;
+  },
+  getById: async (id) => {
+    const book = await Book.findByPk(id)
   }
 }
 
