281dc73f95115ee543577998afff46a45c2e2f62
Andre Felipe <afelipes@gmail.com>
2022-07-08 22:41:44 -0300

    feat: Crie um seeder usando o Sequelize-CLI, populando a sua base com pelo menos um livro;

---
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio/books/src/database/seeders/20220709012935-books.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => await queryInterface.bulkInsert('Books',
+    [
+      {
+        title: 'Livro 1',
+        author: 'Autor 1',
+        page_quantity: 200,
+        created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updated_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      {
+        title: 'Livro 2',
+        author: 'Autor 2',
+        page_quantity: 200,
+        created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updated_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      {
+        title: 'Livro 3',
+        author: 'Autor 3',
+        page_quantity: 200,
+        created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updated_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    ], {}),
+  down: async (queryInterface, Sequelize) => await queryInterface.bulkDelete('Books', null, {}),
+};
