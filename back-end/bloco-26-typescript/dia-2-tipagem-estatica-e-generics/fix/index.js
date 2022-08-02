955bb972ce27c514ef677dd5f87c20117a3b6fcf
Andre Felipe <afelipes@gmail.com>
2022-08-02 15:18:52 -0300

    finalizando dia 26.2 (imcompleto)

---
+++ back-end/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/fix/index.js
@@ -0,0 +1,34 @@
+"use strict";
+let vogais = ['a', 'e', 'i', 'o', 'u'];
+console.log(vogais[0]);
+// let cpf: string | number
+// const states: string[] = ['sólido', 'líquido', 'gasoso']
+// Crie um type para um objeto que represente um pássaro.
+// type Bird = {
+//   name: string,
+// }
+// function printByrd(item: Bird){
+//   console.log(item.name)
+// }
+// printByrd({name: 'Canário'})
+// Crie um type que represente uma função que recebe dois valores numéricos e retorne a soma deles.
+// type Sum = {
+//   x: number,
+//   y: number
+// }
+// function printSum(item: Sum){
+//   console.log(item.x + item.y)
+// }
+// printSum({ x: 456456, y:45684564})
+// Crie um type para um objeto que represente um endereço.
+// type Address = {
+//   street: string,
+//   number: number
+// }
+// function printAddress(item: Address){
+//   console.log(`${item.street}, ${item.number}`)
+// }
+// printAddress({
+//   street: 'Rua dos bobos',
+//   number: 0
+// })
