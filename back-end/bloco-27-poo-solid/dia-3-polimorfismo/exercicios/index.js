273e4345d492bd83ee9064baabd4f85e658cd611
Andre Felipe <afelipes@gmail.com>
2022-08-05 10:54:41 -0300

    feat: Exercicio 1
    Heranças e Interfaces

--- back-end/bloco-27-poo-solid/dia-3-polimorfismo/exercicios/index.js
+++ back-end/bloco-27-poo-solid/dia-3-polimorfismo/exercicios/index.js
@@ -1,22 +1,7 @@
 "use strict";
-class estudantes {
-    constructor(matricula, nome, provas, trabalhos) {
-        this.matricula = matricula;
-        this.nome = nome,
-            this.provas = provas,
-            this.trabalhos = trabalhos;
-    }
-    somaNotas() {
-        const provas = this.provas.reduce((acc, nota) => acc + nota, 0);
-        const trabalhos = this.trabalhos.reduce((acc, nota) => acc + nota, 0);
-        return provas + trabalhos;
-    }
-    mediaNotas() {
-        const media = this.somaNotas() / (this.provas.length + this.trabalhos.length);
-        return media;
-    }
-}
-const andre = new estudantes('007', 'Andre Felipe', [5, 5, 5, 5], [5, 5]);
-console.log(andre);
-console.log('A soma das notas é ', andre.somaNotas());
-console.log('A média das notas é ', andre.mediaNotas());
+Object.defineProperty(exports, "__esModule", { value: true });
+var Persons_1 = require("./Persons");
+var maria = new Persons_1.default('Maria da Conceição', new Date('1980/01/25'));
+var luiza = new Persons_1.default('Luiza Andrade', new Date('1965/10/02'));
+console.log(maria);
+console.log(luiza);
