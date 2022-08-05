51c0aefd806f169cd4d8c7ff29977bce7271e440
Andre Felipe <afelipes@gmail.com>
2022-08-05 07:24:46 -0300

    feat: Software Escolar

---
+++ back-end/bloco-27-poo-solid/dia-3-polimorfismo/exercicios/index.ts
@@ -0,0 +1,36 @@
+class estudantes {
+  matricula: string;
+  nome: string;
+  provas: number[]
+  trabalhos: number[]
+  
+  constructor (
+    matricula: string,
+    nome: string,
+    provas: number[],
+    trabalhos: number[],
+  ){
+    this.matricula = matricula;
+    this.nome = nome,
+    this.provas = provas,
+    this.trabalhos = trabalhos
+  }
+
+  somaNotas() {
+    const provas =  this.provas.reduce((acc, nota) => acc + nota, 0 );
+    const trabalhos = this.trabalhos.reduce((acc, nota) => acc + nota, 0 )
+    return provas + trabalhos;
+  }
+
+  mediaNotas() {
+    const media = this.somaNotas() / (this.provas.length + this.trabalhos.length);
+    return media;
+  }
+}
+
+const andre = new estudantes('007', 'Andre Felipe', [5,5,5,5], [5,5]);
+
+console.log(andre);
+
+console.log('A soma das notas é ', andre.somaNotas());
+console.log('A média das notas é ', andre.mediaNotas());
