53bcc96da73a28e9a41305fbf7a08c987adb7128
Andre Felipe <afelipes@gmail.com>
2022-09-14 11:45:24 -0300

    feat: Exercício 4:
    🚀 Faça o teste de unidade do método read referente classe Frame.

--- src/tests/unit/models/frame.test.ts
+++ src/tests/unit/models/frame.test.ts
@@ -5,13 +5,16 @@ import sinon from 'sinon';
 import FrameModel from '../../../models/Frame';
 import { Model } from 'mongoose';
 import { frameMock, frameMockWithId } from '../../mocks/frameMock';
+import IFrame from '../../../interfaces/Frame';
 
 describe('Frame Model', () => {
   const frameModel = new FrameModel();
+  const frameList = [frameMockWithId, {id: 'xxx', material: 'Prata', color: 'De outro naipe'}];
 
 	before(() => {
 		sinon.stub(Model, 'create').resolves(frameMockWithId);
 		sinon.stub(Model, 'findOne').resolves(frameMockWithId);
+    sinon.stub(Model, 'find').resolves(frameList);
 	});
 
 	after(() => {
@@ -35,9 +38,19 @@ describe('Frame Model', () => {
 			try {
 				await frameModel.readOne('123ERRADO');
 			} catch (error: any) {
-				expect(error.message).to.be.eq('InvalidMongoId');
+				expect(error.message).to.be.eq('Invalid MongoId');
 			}
 		});
 	});
+
+  describe('searching frames', () => {
+    it('', async () => {
+      const frameFound = await frameModel.read();
+      expect(frameFound).to.be.an('array');
+      frameFound?.forEach((frame: IFrame, index: number) => {
+        expect(frame).to.be.deep.equal(frameList[index]);
+      })
+    })
+  })
 
 });
