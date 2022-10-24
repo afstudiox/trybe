cb9d91027ab14804863c8bc6d9c1ca62f1ef292f
Andre Felipe <afelipes@gmail.com>
2022-10-24 09:52:56 -0300

    init: init day study

--- cs/secao-1-dia-01/ex1.py
+++
@@ -1,8 +0,0 @@
-def maior_numero(n1, n2):
-    if n1 > n2:
-        return n1
-    else:
-        return n2
-
-
-print(maior_numero(50, 20))
--- cs/secao-1-dia-01/ex2.py
+++
@@ -1,5 +0,0 @@
-import random
-
-valores = random.sample(range(0, 100), 10)
-
-print(valores)
