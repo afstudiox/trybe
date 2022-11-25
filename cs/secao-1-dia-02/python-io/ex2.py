ebe1bedaad566de90a69a6d199cdf202238ab4ee
Andre Felipe <afelipes@gmail.com>
2022-11-25 18:24:32 -0300

    feat:

--- cs/secao-1-dia-02/python-io/ex2.py
+++
@@ -1,9 +0,0 @@
-soma = 0
-values = input(
-    'Digite os valores a serem somados, separando-os por vigula(,): ')
-for item in values.split(','):
-    if not item.isdigit():
-        print(f'Erro ao somar valores, {item} não é um número')
-    else:
-        soma += int(item)
-print(f'A soma dos valores válidos é: {soma}')
